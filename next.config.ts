afb40c785068bd65a93144f67568bf5979d285a3
Haard2705 <haard.pathak@gmail.com>
2025-07-22 10:53:37 -0400

    upd_next_config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["mcoamsifdvhsrkubniax.supabase.co"],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
